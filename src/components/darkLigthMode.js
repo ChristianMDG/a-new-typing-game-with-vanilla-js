118539b3b4e20cc7215fac353275d4de608a33da
nassai <hei.nassai@gmail.com>
2025-04-15 09:16:23 +0300

    add file js darkLigthMode

---
+++ src/components/darkLigthMode.js
@@ -0,0 +1,12 @@
+const darkModeToggle = document.getElementById('dark-mode-toggle');
+
+
+// Toggle dark/light mode
+const toggleDarkMode = () => {
+    document.body.classList.toggle('light-mode');
+    const isLightMode = document.body.classList.contains('light-mode');
+    localStorage.setItem('theme', isLightMode ? 'light' : 'dark');
+}
+
+// Dark mode toggle
+darkModeToggle.addEventListener('change', toggleDarkMode);
