78e51d9b27d85af7fbb4e97f33eb40fe99aff30f
ChristianMDG <christian.ravelojaona@student.passerellesnumeriques.org>
2025-04-17 16:00:04 +0300

    build animation from login page

--- src/components/typing_test_modules/utils.js
+++
@@ -1,5 +0,0 @@
-export function toggleDarkMode() {
-    document.body.classList.toggle('light-mode');
-    const isLightMode = document.body.classList.contains('light-mode');
-    localStorage.setItem('theme', isLightMode ? 'light' : 'dark');
-}
